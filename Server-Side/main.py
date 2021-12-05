33837b0fb1cfb9c06fd5b2bc579b8b875be02967
Muhammadqodir <M-Abduvoitov@mail.ru>
2021-12-05 04:39:28 +0300

    Created listener for client responces in server side

--- Server-Side/main.py
+++ Server-Side/main.py
@@ -3,18 +3,9 @@ from firebase_admin import credentials
 from firebase_admin import db
 from firebase_admin import storage
 import requests
-import wget
 import datetime
-
-def download_file(url):
-	local_filename = "test.3gp"
-	# NOTE the stream=True parameter below
-	with requests.get(url, stream=True) as r:
-		r.raise_for_status()
-		with open(local_filename, 'wb') as f:
-			for chunk in r.iter_content(chunk_size=8192):
-				f.write(chunk)
-	return local_filename
+import json
+import pyperclip
 
 cred = credentials.Certificate("mybaby-d3065-firebase-adminsdk-2119n-ba1b958d33.json")
 firebase_admin.initialize_app(cred, {
@@ -23,11 +14,30 @@ firebase_admin.initialize_app(cred, {
 	})
 bucket = storage.bucket('mybaby-d3065.appspot.com')
 
-# blob = bucket.blob("R8I55usQTEbv.3gp")
-# download_url = blob.generate_signed_url(datetime.timedelta(seconds=300), method='GET')
-# download_file(download_url)
+
+def download_file(fileName):
+	blob = bucket.blob(fileName)
+	download_url = blob.generate_signed_url(datetime.timedelta(seconds=300), method='GET')
+	pyperclip.copy(download_url)
+	local_filename = "./downloads/"+fileName
+	# NOTE the stream=True parameter below
+	with requests.get(download_url, stream=True) as r:
+		r.raise_for_status()
+		with open(local_filename, 'wb') as f:
+			for chunk in r.iter_content(chunk_size=8192):
+				f.write(chunk)
+	return local_filename
 
 def listener(event):
 	print(event.data)
+	try:
+		action = event.data["action"]
+		if action == "recognize":
+			fileName = event.data["fileName"]
+			download_file(fileName)
+			print("File was downloaded successfully!")
+	except Exception as e:
+		print(e)
+		print("ok")
 
 ref = db.reference("/requests").listen(listener)
